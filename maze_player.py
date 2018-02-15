ce9ee188ad7b29d7efaa12190e6af1ec6ad30329
Ken-Yi Lee <kenyilee@gmail.com>
2018-02-15 10:34:07 +0800

    Set frame rate

--- maze_player.py
+++ maze_player.py
@@ -6,11 +6,26 @@ import pygame
 def main():
     pygame.init()
     pygame.display.set_mode((320, 240))
+    pygame.display.set_caption('Maze Player')
+
+    target_fps = 30.
+    target_frame_duration = 1000. / target_fps
+
+    last_ticks = 0
+
+    clock = pygame.time.Clock()
+
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
         pygame.display.flip()
-        pygame.time.delay(100)
+        current_ticks = pygame.time.get_ticks()
+        elapsed_duration = current_ticks - last_ticks
+        if elapsed_duration < target_frame_duration:
+            pygame.time.delay(int(target_frame_duration-elapsed_duration))
+        last_ticks = pygame.time.get_ticks()
+        clock.tick()
+        print(clock.get_fps())
     pass
 
 
